d3a5260bbe9348656d74c7e351ea70ba690bc555
sahalsajjad <sahalsajjad@gmail.com>
2016-04-10 15:02:27 +0530

    PasswordChange and Signout completed

--- feasta/forms.py
+++ feasta/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import *
+from django.contrib.auth.forms import AuthenticationForm 
 from feasta.config import model_choices
 class LoginForm(AuthenticationForm):
 	username = forms.CharField(
@@ -57,3 +57,64 @@ class BulkUnregisterForm(forms.Form):
 				'class':'form-control feasta-blackfont',
 			}
 		))
+
+class SetPasswordForm(forms.Form):
+    error_messages = {
+        'password_mismatch': ("The two password fields didn't match."),
+    }
+    new_password1 = forms.CharField(label=("New password"),
+                                    widget=forms.PasswordInput(
+                                    	attrs={'class':'form-control'}
+                                    	))
+    new_password2 = forms.CharField(label=("New password confirmation"),
+                                    widget=forms.PasswordInput(
+                                    	attrs={'class':'form-control'}
+                                    	))
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(SetPasswordForm, self).__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code='password_mismatch',
+                )
+        
+        return password2
+
+    def save(self, commit=True):
+        password = self.cleaned_data["new_password1"]
+        self.user.set_password(password)
+        if commit:
+            self.user.save()
+        return self.user
+
+
+class PasswordChangeForm(SetPasswordForm):
+    error_messages = dict(SetPasswordForm.error_messages, **{
+        'password_incorrect': ("Your old password was entered incorrectly. "
+                                "Please enter it again."),
+    })
+    old_password = forms.CharField(label=("Old password"),
+                                   widget=forms.PasswordInput(
+                                    	attrs={'class':'form-control'}
+                                    	))
+
+    field_order = ['old_password', 'new_password1', 'new_password2']
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data["old_password"]
+        if not self.user.check_password(old_password):
+            raise forms.ValidationError(
+                self.error_messages['password_incorrect'],
+                code='password_incorrect',
+            )
+        return old_password
+
+
+
--- feasta/urls.py
+++ feasta/urls.py
@@ -1,16 +1,42 @@
 from django.conf.urls import url
 from feasta.views import *
+from feasta.forms import PasswordChangeForm
 from django.conf import settings
+from django.contrib.auth.views import logout, password_change
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-			url(r'^$', login_required(Home.as_view())),
-			url(r'^accounts/login/$', Login.as_view()),
-			url(r'^unregister/$', login_required(UnregisterView.as_view())),
-			url(r'^bulkunregister/$', login_required(BulkUnregisterView.as_view())),
-			url(r'^menu/$', login_required(MenuView.as_view())),
-			url(r'^accounts/signout/$', login_required(signout)),
-			url(r'^accounts/change-password/$', login_required(changePassword)),
-			url(r'^accounts/edit-profile/$', login_required(EditProfile.as_view())),
-			url(r'^getlist/today/$', login_required(ListForMeal.as_view()))
+			url(r'^$',
+				login_required(Home.as_view())),
+
+			url(r'^accounts/login/$', 
+				Login.as_view()),
+
+			url(r'^unregister/$', 
+				login_required(UnregisterView.as_view())),
+
+			url(r'^bulkunregister/$', 
+				login_required(BulkUnregisterView.as_view())),
+
+			url(r'^menu/$', 
+				login_required(MenuView.as_view())),
+
+			url(r'^accounts/edit-profile/$', 
+				login_required(EditProfile.as_view())),
+
+			url(r'^getlist/today/$', 
+				login_required(ListForMeal.as_view())),
+
+			url(r'^accounts/signout/$', 
+				login_required(logout), 
+				kwargs={'next_page':settings.LOGOUT_URL },	
+				name='logout'), 
+
+			url(r'^accounts/change-password/$', 
+				login_required(password_change), 
+				name='passwordchange',
+				kwargs={'post_change_redirect':settings.LOGIN_URL,
+					    'template_name':'passwordchange.html',
+					    'password_change_form':PasswordChangeForm
+				})
 		]
